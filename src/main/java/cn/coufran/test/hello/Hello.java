a3ccd1d661221107aad7744297715fe193031200
liuhuiming <liuhuiming@dotax.cn>
2020-05-02 21:08:48 +0800

    修改：源码

---
+++ src/main/java/cn/coufran/test/hello/Hello.java
@@ -0,0 +1,9 @@
+package cn.coufran.test.hello;
+
+public class Hello {
+    public static String say(String name) {
+        String word = "Hello, " + name + "!";
+        System.out.println(word);
+        return word;
+    }
+}
--- src/main/java/cn/coufran/test/hello/HelloMaven.java
+++
@@ -1,7 +0,0 @@
-package cn.coufran.test.hello;
-
-public class HelloMaven {
-    public static void main(String[] args) {
-        System.out.println("Hello,Maven");
-    }
-}
