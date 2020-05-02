15c2e7669d493bc47264f31cc3dc82e557e5f6f9
liuhuiming <liuhuiming@dotax.cn>
2020-05-02 21:48:11 +0800

    增加：注释

--- src/main/java/cn/coufran/test/hello/Hello.java
+++ src/main/java/cn/coufran/test/hello/Hello.java
@@ -1,6 +1,16 @@
 package cn.coufran.test.hello;
 
+/**
+ * Hello发言者
+ * @author coufran
+ * @since 1.0.0
+ */
 public class Hello {
+    /**
+     * Say Hello
+     * @param name Hello name
+     * @return Hello word
+     */
     public static String say(String name) {
         String word = "Hello, " + name + "!";
         System.out.println(word);
